baea816ced9a3ba1ecf5fbc2e1bddc83c9ea79b4
Oscar <oscarodriguez56@gmail.com>
2025-08-08 22:19:40 +0200

    Charging all Rust project, I was so concentrated in develop that I forget to do the commits I should do

---
+++ src/main.rs
@@ -0,0 +1,39 @@
+use std::io;
+// It's possible define in the program that we use std::io to avoid the need of reference the standard library in the program
+
+fn obtain_number(prompt: &str, custom_err: &str) -> i32 {
+    let mut inpt = String::new();
+    print!("{}",prompt);
+    io::stdin().read_line(&mut inpt).expect(custom_err);
+    return inpt.trim().parse::<i32>().unwrap_or(0); // I did this to simplify the normal thing is to use Result and things like that but I want something simple and the 0 doesn't change anything in an operation
+}
+fn operate(operation: &str, num1: i32, num2:i32) -> i32 {
+    match operation {
+        "+" => num1 + num2,
+        "-" => num1 - num2,
+        "*" => num1 * num2,
+        "/" => num1 / num2,
+        "mod" => num1 % num2,
+        _ => {
+            println!("A correct operation wasn't defined");
+            0
+        }
+    }
+}
+fn select_operator() -> String {
+    print!("Select an operator: +, -, *, /, mod");
+    let mut operator = String::new();
+    io::stdin().read_line(&mut operator).unwrap();
+    return operator.trim().to_string();
+}
+
+fn main() {
+    let num1 = obtain_number("Write the first number: ", "It wasn't possible to read first number");
+    let num2 = obtain_number("Write the second number: ", "It wasn't possible to read second number");
+    let operator = select_operator();
+    println!("The result is {}", operate(operator.as_str(),num1,num2))
+}
+
+
+
+
